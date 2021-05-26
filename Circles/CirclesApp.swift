d46a634856f0cc05ba76b1a41ec8312aa49b50bf
Charles Wright <cvwright@kombucha.systems>
2021-05-25 20:43:19 -0500

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Macro Ramius on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
