15c1fd4d64358514bddb968b432ecf3412e6fff1
Charles Wright <cvwright@kombucha.systems>
2021-06-18 20:32:32 -0500

    End of week updates.  This is beta v0.15 (2).

--- Circles/Views/Timeline/MessageDetailSheet.swift
+++ Circles/Views/Timeline/MessageDetailSheet.swift
@@ -2,19 +2,28 @@
 //  MessageDetailSheet.swift
 //  Circles
 //
-//  Created by Macro Ramius on 6/14/21.
+//  Created by Charles Wright on 6/14/21.
 //
 
 import SwiftUI
 
 struct MessageDetailSheet: View {
+    @ObservedObject var message: MatrixMessage
+    var displayStyle: MessageDisplayStyle = .timeline
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MessageCard(message: message, displayStyle: displayStyle)
+
+            Text("Type: \(message.type)")
+            Text("Relates to: \(message.relatesToId ?? "none")")
+        }
     }
 }
 
+/*
 struct MessageDetailSheet_Previews: PreviewProvider {
     static var previews: some View {
         MessageDetailSheet()
     }
 }
+*/
