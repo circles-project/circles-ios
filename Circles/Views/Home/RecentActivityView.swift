411a1b04728d6a666248159f3b1b5d4fbe2c00ba
Charles Wright <cvwright@kombucha.systems>
2021-09-01 09:46:36 -0500

    Cleaned up the code for Recent Activity view, and made it automatically refresh its data on appear

--- Circles/Views/Home/RecentActivityView.swift
+++ Circles/Views/Home/RecentActivityView.swift
@@ -15,89 +15,94 @@ struct RecentActivityView: View {
     @State var recentCircles: [SocialCircle] = []
     @State var recentGroups: [SocialGroup] = []
 
-    /*
-    init(store: KSStore) {
-        self.store = store
-        self.recentCircles = store.getCircles().filter {
+    @State var pending = true
+
+    func refreshData() {
+        recentCircles = store.getCircles().filter {
             $0.stream.latestMessage != nil
         }
-        self.recentGroups = store.getGroups().groups.filter {
+        recentGroups = store.getGroups().groups.filter {
             $0.room.last != nil
         }
     }
-    */
 
-    var body: some View {
-        // Having multiple ScrollViews one inside the other makes things wonky
-        // We're making the HomeScreen the only one with the ScrollView now
-        //ScrollView {
-            VStack(alignment: .leading) {
-
-                if recentCircles.isEmpty && recentGroups.isEmpty {
-                    HStack {
-                        Spacer()
-                        VStack {
-                            Text("No recent activity to display")
-                                .padding()
-                            Button(action: {
-                                recentCircles = store.getCircles().filter {
-                                    $0.stream.latestMessage != nil
-                                }
-                                recentGroups = store.getGroups().groups.filter {
-                                    $0.room.last != nil
-                                }
-                            }) {
-                                Label("Refresh", systemImage: "arrow.clockwise.circle")
-                            }
-                            .padding()
-                        }
-                        Spacer()
-                    }
-                }
-
-                ForEach(recentCircles) { circle in
-                    if let message = circle.stream.latestMessage {
-                        Text("From Circle: \(circle.name)")
-                            //.font(.caption)
-                            .fontWeight(.bold)
-                            .padding(.top, 2)
-                        Button(action: {
-                            // Set the tab to be "Circles"
-                            // Set the selected circle to be this one
-                        }) {
-                            MessageCard(message: message, displayStyle: .timeline)
-                                .padding(.leading)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                    }
-                }
-                ForEach(recentGroups) { group in
-                    if let message = group.room.last {
-                        Text("From Group: \(group.room.displayName ?? "(unnamed group)")")
-                            //.font(.caption)
-                            .fontWeight(.bold)
-                            .padding(.top, 2)
+    var activityList: some View {
+        VStack {
+            ForEach(recentCircles) { circle in
+                if let message = circle.stream.latestMessage {
+                    Text("From Circle: \(circle.name)")
+                        //.font(.caption)
+                        .fontWeight(.bold)
+                        .padding(.top, 2)
+                    Button(action: {
+                        // Set the tab to be "Circles"
+                        // Set the selected circle to be this one
+                    }) {
                         MessageCard(message: message, displayStyle: .timeline)
                             .padding(.leading)
                     }
+                    .buttonStyle(PlainButtonStyle())
                 }
-            //}
             }
-            .onAppear {
-                /*
-                let circles = store.getCircles()
-                self.recentCircles = circles.filter {
-                    $0.stream.latestMessage != nil
+            ForEach(recentGroups) { group in
+                if let message = group.room.last {
+                    Text("From Group: \(group.room.displayName ?? "(unnamed group)")")
+                        //.font(.caption)
+                        .fontWeight(.bold)
+                        .padding(.top, 2)
+                    MessageCard(message: message, displayStyle: .timeline)
+                        .padding(.leading)
+                }
+            }
+        }
+    }
+
+    var refreshView: some View {
+        VStack {
+            Spacer()
+
+            HStack {
+                Spacer()
+
+                if pending {
+                    ProgressView()
+                } else {
+                    VStack {
+                        Text("No recent activity to display")
+                            .padding()
+                        Button(action: {
+                            self.refreshData()
+                        }) {
+                            Label("Refresh", systemImage: "arrow.clockwise.circle")
+                        }
+                        .padding()
+                    }
                 }
 
-                let groups = store.getGroups().groups
-                self.recentGroups = groups.filter {
-                    $0.room.last != nil
-                }
-                */
+                Spacer()
             }
-        //.navigationBarTitle(Text("Recent Activity"))
-        //.padding()
+            Spacer()
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            if recentCircles.isEmpty && recentGroups.isEmpty {
+                refreshView
+            } else {
+                activityList
+            }
+        }
+        .onAppear {
+            if recentCircles.isEmpty && recentGroups.isEmpty {
+                // Maybe the UI loaded before the data was ready
+                // Wait a couple of seconds and try again
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.refreshData()
+                    self.pending = false
+                }
+            }
+        }
     }
 }
 
