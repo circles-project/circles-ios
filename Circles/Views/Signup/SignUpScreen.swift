c0436ae5862f50b38f2bb5fca2379a3a6fbcc30d
Charles Wright <cvwright@futo.org>
2023-04-03 15:12:56 -0500

    Removing old comments

--- Circles/Views/Signup/SignUpScreen.swift
+++ Circles/Views/Signup/SignUpScreen.swift
@@ -114,64 +114,6 @@ struct SignupScreen: View {
         cancelButton
     }
 
-    /*
-    var body: some View {
-        VStack {
-            if let flow = selectedFlow {
-
-                if let stage = flow.stages.first {
-                    switch stage {
-                    case LOGIN_STAGE_TOKEN_KOMBUCHA,
-                         LOGIN_STAGE_TOKEN_MATRIX,
-                         LOGIN_STAGE_TOKEN_MSC3231:
-                        TokenForm(tokenType: stage, session: session, authFlow: $selectedFlow)
-                    case LOGIN_STAGE_TERMS_OF_SERVICE:
-                        TermsOfServiceForm(session: session, authFlow: $selectedFlow)
-                    case LOGIN_STAGE_VERIFY_EMAIL:
-                        if emailSessionId != nil {
-                            ValidateEmailForm(matrix: matrix, authFlow: $selectedFlow, emailSid: $emailSessionId, accountInfo: $accountInfo, creds: $creds)
-                        } else {
-                            AccountInfoForm(matrix: matrix, authFlow: $selectedFlow, stage: stage, accountInfo: $accountInfo, emailSid: $emailSessionId)
-                        }
-                    case LOGIN_STAGE_APPLE_SUBSCRIPTION:
-                        AppStoreSubscriptionForm(matrix: matrix, uiaaState: $uiaaState, authFlow: $selectedFlow)
-                    default:
-                        Text("Stage is [\(stage)]")
-                    }
-                } else {
-                    let stage = postSignupFlow.stages.first
-                    switch stage {
-                    case "avatar":
-                        AvatarForm(matrix: matrix, pseudoFlow: $postSignupFlow)
-                    case "circles":
-                        CirclesForm(matrix: matrix, displayName: accountInfo.displayName, pseudoFlow: $postSignupFlow)
-                    case nil:
-                        AllDoneForm(matrix: matrix, userId: accountInfo.userId, uiaaState: $uiaaState)
-                    default:
-                        Text("Stage is [\(stage ?? "(none)")]")
-                    }
-
-                }
-                
-            } else {
-                SignupStartForm(matrix: matrix, uiaaState: $uiaaState, selectedFlow: $selectedFlow)
-            }
-
-        }
-        .onAppear {
-            // Start the process of fetching the App Store products, so that it will be loaded by the time the user decides to tap the App Store button
-            if appStore.membershipProducts.isEmpty {
-                // Get the productIds from the initial UIAA state
-                // Ask the App Store interface to load information on them
-                if let params = uiaaState?.params?["org.futo.subscription.apple"] as? AppleSubscriptionParams {
-                    let productIds = params.productIds
-                    appStore.fetchProducts(matchingIdentifiers: productIds)
-                }
-            }
-        }
-    }
-     */
-
 }
 
 /*
