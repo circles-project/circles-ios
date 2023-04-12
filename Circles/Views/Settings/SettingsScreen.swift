f4269587ef238cf0317c4dc8e3fe852b40a8132a
Charles Wright <cvwright@futo.org>
2023-04-12 14:59:11 -0500

    More rough outline of what the Settings screen should look like

--- Circles/Views/Settings/SettingsScreen.swift
+++ Circles/Views/Settings/SettingsScreen.swift
@@ -12,12 +12,40 @@ import Matrix
 
 struct SettingsScreen: View {
     @ObservedObject var session: CirclesSession
+    var user: Matrix.User
+    
+    init(session: CirclesSession) {
+        self.session = session
+        self.user = session.matrix.getUser(userId: session.matrix.creds.userId)
+    }
+    
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Settings")
-            Spacer()
+        NavigationView {
+            Form {
+                Section(header: Label("Public Profile", systemImage: "person.crop.circle.fill")) {
+                    MessageAuthorHeader(user: user)
+                    Text("Status Message")
+                    Text("Neopass")
+                }
+                Section(header: Label("Account Security", systemImage: "lock.fill")) {
+                    Text("Password")
+                    Text("Email")
+                    Text("Login Sessions")
+                }
+                Section(header: Text("Subscription")) {
+                    Text("Expiration Date")
+                    Text("Cloud Storage")
+                }
+                Section(header: Text("Other Stuff")) {
+                    Text("Notifications")
+                    Text("Version")
+                }
+                Section(header: Label("Danger Zone", systemImage: "exclamationmark.triangle")) {
+                    Text("Deactivate Account")
+                }
+            }
+            .navigationTitle("Settings")
         }
     }
 }
