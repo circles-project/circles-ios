4c6a045f5f3c2fdd09cfb8f90310e3332d93433f
Charles Wright <cvwright@futo.org>
2023-07-26 16:13:16 -0500

    Added UI to configure the user's avatar image -- Fixes #34

--- Circles/Views/Settings/ProfileSettingsView.swift
+++ Circles/Views/Settings/ProfileSettingsView.swift
@@ -6,26 +6,63 @@
 //
 
 import SwiftUI
+import PhotosUI
 import Matrix
 
 struct ProfileSettingsView: View {
     @ObservedObject var session: Matrix.Session
     //@ObservedObject var user: Matrix.User
     
+    @State var showPicker = false
+    @State var newAvatarImageItem: PhotosPickerItem?
+    
     var body: some View {
         VStack {
+            /*
             VStack {
-                ProfileImageView(user: session.me)
+                Image(uiImage: session.avatar ?? UIImage(systemName: "person.fill")!)
+                    .resizable()
+                    .scaledToFill()
                     .clipShape(Circle())
-                Text("\(session.displayName ?? session.creds.userId.stringValue)")
+                    .frame(width: 200, height: 200)
+                    //.padding(-30)
+                Text("\(session.displayName ?? "@\(session.creds.userId.username)")")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text(session.creds.userId.stringValue)
             }
+            */
             Form {
-                Text("Profile picture")
+                HStack {
+                    Text("Profile picture")
+                    Spacer()
+                    
+                    PhotosPicker(selection: $newAvatarImageItem, matching: .images) {
+                        Image(uiImage: session.avatar ?? UIImage(systemName: "person.fill")!)
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                            .frame(width: 80, height: 80)
+                    }
+                    .buttonStyle(.plain)
+                    .onChange(of: newAvatarImageItem) { _ in
+                        Task {
+                            if let data = try? await newAvatarImageItem?.loadTransferable(type: Data.self) {
+                                if let img = UIImage(data: data) {
+                                    try await session.setMyAvatarImage(img)
+                                }
+                            }
+                        }
+                    }
+                }
                 
                 NavigationLink(destination: UpdateDisplaynameView(session: session)) {
                     Text("Your name")
                         .badge(session.displayName ?? "(none)")
                 }
+                
+                Text("User ID")
+                    .badge(session.creds.userId.stringValue)
                 
                 NavigationLink(destination: UpdateStatusMessageView(session: session)) {
                     Text("Status message")
