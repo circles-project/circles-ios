e99d036f65ce9a0ff5fb340e003915bdd2d7a7ce
Charles Wright <cvwright@futo.org>
2023-12-13 12:06:19 -0600

    Make the photo gallery view refreshable

--- Circles/Views/Photo Galleries/GalleryGridView.swift
+++ Circles/Views/Photo Galleries/GalleryGridView.swift
@@ -144,6 +144,24 @@ struct GalleryGridView: View {
             .onAppear {
                 print("GalleryGridView: Found \(messages.count) image/video messages")
             }
+            .refreshable {
+                print("REFRESH\tGetting latest messages for room \(room.name ?? room.roomId.stringValue)")
+                if let msgs = try? await room.getMessages(forward: false) {
+                    print("REFRESH\tGot response from server")
+                }
+                
+                print("REFRESH\tUpdating room state")
+                room.updateAvatarImage()
+                
+                print("REFRESH\tSleeping to let network requests come in")
+                try? await Task.sleep(for: .seconds(1))
+                
+                print("REFRESH\tSending Combine update")
+                await MainActor.run {
+                    room.objectWillChange.send()
+                }
+                print("REFRESH\tDone")
+            }
 
         }
         .gesture(MagnificationGesture()
