54044ce86849c87f69e17302ff35d2b15cc1100f
Charles Wright <cvwright@futo.org>
2024-03-19 12:47:32 -0500

    Cleanup - Remove old comments at the top level of the app

--- Circles/CirclesApp.swift
+++ Circles/CirclesApp.swift
@@ -27,27 +27,6 @@ struct CirclesApp: App {
         WindowGroup {
             ContentView(store: store)
                 .environmentObject(store)
-                /*
-                .environmentObject(iapObserver)
-                .onAppear {
-
-                    
-                    // For some strange reason, I'm getting nil for the storefront on the first run of the app.
-                    // What if we just do some thing really stupid here and ask for it here well before we actually need it?
-                    if let storefront = SKPaymentQueue.default().storefront {
-                        let countryCode = storefront.countryCode
-                        print("APP\tGot country code = \(countryCode)")
-                    } else {
-                        print("APP\tCouldn't get country code from StoreKit")
-                    }
-
-                    SKPaymentQueue.default().add(iapObserver)
-
-                }
-                .onDisappear {
-                    SKPaymentQueue.default().remove(iapObserver)
-                }
-                */
         }
     }
 }
