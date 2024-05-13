6271d542c6e11a5bde37ee21ea39ef968734b7ad
Charles Wright <cvwright@futo.org>
2024-05-13 15:06:45 -0500

    Cleanup the photo gallery creation sheet, based on the group creation sheet

--- Circles/Views/Groups/GroupCreationSheet.swift
+++ Circles/Views/Groups/GroupCreationSheet.swift
@@ -84,7 +84,7 @@ struct GroupCreationSheet: View {
         VStack {
             buttonbar
             let frameWidth = 300.0
-            let frameHeight = 150.0
+            let frameHeight = 200.0
       
             ZStack {
                 if let img = self.headerImage {
@@ -95,6 +95,7 @@ struct GroupCreationSheet: View {
 
                 } else {
                     Color.gray
+                        .frame(width: frameWidth, height: frameHeight)
                 }
 
                 VStack {
--- Circles/Views/Photo Galleries/PhotoGalleryCreationSheet.swift
+++ Circles/Views/Photo Galleries/PhotoGalleryCreationSheet.swift
@@ -50,108 +50,91 @@ struct PhotoGalleryCreationSheet: View {
     }
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                buttonBar
+        VStack(spacing: 10) {
+            buttonBar
+            let frameWidth: CGFloat = 300
+            let frameHeight: CGFloat = 200
                 
-                Text("New Photo Gallery")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                
-                Spacer()
-                
-                let frameWidth: CGFloat = 300
-                let frameHeight: CGFloat = 150
-                    
-                PhotosPicker(selection: $selectedItem, matching: .images) {
-                    if let img = avatarImage {
-                        ZStack {
-                            Image(uiImage: img)
-                                .resizable()
-                                .scaledToFill()
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .background(RoundedRectangle(cornerRadius: 10)
-                                            //.stroke(Color.gray, lineWidth: 2)
-                                    .stroke(Color.gray)
-                                    .foregroundColor(.background)
-                                )
-                                .padding()
-                            
-                            Text(galleryName)
-                                .lineLimit(2)
-                                .font(.title2)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .shadow(color: .black, radius: 10)
-                        }
+            ZStack {
+                if let img = avatarImage {
+                    Image(uiImage: img)
+                        .resizable()
+                        .scaledToFill()
                         .frame(width: frameWidth, height: frameHeight)
-
-                    } else {
-                        Image(systemName: "camera.on.rectangle")
-                            .resizable()
-                            .scaledToFit()
-                            .padding()
-                            .frame(width: frameWidth, height: frameHeight)
-                            .background(RoundedRectangle(cornerRadius: 10)
-                                        //.stroke(Color.gray, lineWidth: 2)
-                                .stroke(Color.gray, style: StrokeStyle(lineWidth: 5, dash: [10, 10]))
-                                .foregroundColor(.background)
-                            )
-                            .padding()
-                    }
+                } else {
+                    Color.gray
+                        .frame(width: frameWidth, height: frameHeight)
                 }
-                .onChange(of: selectedItem) { newItem in
-                    Task {
-                        print("Handling a new item")
-                        if let data = try? await newItem?.loadTransferable(type: Data.self),
-                           let img = UIImage(data: data)
-                        {
-                            await MainActor.run {
-                                self.avatarImage = img
-                            }
-                        }
-                    }
-                }
-                
-                TextField("Gallery name", text: $galleryName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .focused($inputFocused)
-                    .frame(width: 300)
-                    .onAppear {
-                        self.inputFocused = true
-                    }
-                
-                HStack {
-                    Text("Default user role")
-                    Spacer()
-                    Picker("User permissions", selection: $defaultPowerLevel) {
-                        ForEach(CIRCLES_POWER_LEVELS) { level in
-                            Text(level.description)
-                                .tag(level)
-                        }
-                    }
-                }
-                .frame(width: 300)
-
-                Spacer()
-                
-                AsyncButton(action: {
-                    try await create()
-                }) {
-                    Text("Create gallery \(galleryName)")
-                        .padding()
-                        .frame(width: 300.0, height: 40.0)
-                        .foregroundColor(.white)
-                        .background(Color.accentColor)
-                        .cornerRadius(10)
-                }
-                .disabled(galleryName.isEmpty)
-                
-                Spacer()
+                    
+                Text(galleryName)
+                    .lineLimit(2)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .shadow(color: .black, radius: 10)
             }
-            .padding()
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay(alignment: .bottomTrailing) {
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    Image(systemName: "pencil.circle.fill")
+                        .symbolRenderingMode(.multicolor)
+                        .font(.system(size: 30))
+                        .foregroundColor(.accentColor)
+                }
+                .buttonStyle(.borderless)
+            }
+            .onChange(of: selectedItem) { newItem in
+                Task {
+                    print("Handling a new item")
+                    if let data = try? await newItem?.loadTransferable(type: Data.self),
+                       let img = UIImage(data: data)
+                    {
+                        await MainActor.run {
+                            self.avatarImage = img
+                        }
+                    }
+                }
+            }
             
+            TextField("Gallery name", text: $galleryName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .textInputAutocapitalization(.words)
+                .focused($inputFocused)
+                .frame(maxWidth: 350)
+                .onAppear {
+                    self.inputFocused = true
+                }
+            
+            HStack {
+                Text("Default user role")
+                Spacer()
+                Picker("User permissions", selection: $defaultPowerLevel) {
+                    ForEach(CIRCLES_POWER_LEVELS) { level in
+                        Text(level.description)
+                            .tag(level)
+                    }
+                }
+            }
+            .frame(width: 300)
+
+            Spacer()
+            
+            AsyncButton(action: {
+                try await create()
+            }) {
+                Text("Create gallery \(galleryName)")
+                    .padding()
+                    .frame(width: 300.0, height: 40.0)
+                    .foregroundColor(.white)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+            }
+            .disabled(galleryName.isEmpty)
+            
+            Spacer()
         }
+        .padding()
+        
     }
 }
 
