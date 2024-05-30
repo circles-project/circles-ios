18ec038bc52a2629d24e172ab54552babe65fb21
Charles Wright <cvwright@futo.org>
2024-05-30 16:31:04 -0500

    Clean up the circles help text

--- Circles/Views/Help/CirclesHelpView.swift
+++ Circles/Views/Help/CirclesHelpView.swift
@@ -15,10 +15,10 @@ struct CirclesHelpView: View {
                 
         Tip: A **circle** works like a secure, private version of Facebook or Twitter.  Everyone posts to their own timeline, and you see posts from all the timelines that you're following.
         
-        A circle is a good way to share things with lots of people who don't all know each other, but they all know you.
+        A circle is a good way to share things with lots of people who don't all know each other, but who all know you.
         
-        For example, think about all the aunts and uncles and cousins from the different sides of your family.
-        Or, think about all of your friends across all of the places you've ever lived.
+        For example, you may have lots of aunts and uncles and cousins from the different sides of your family.
+        Or, you might have several friends from many different places where you've lived.
 
         If you want to connect a bunch of people who *do* all know each other, then it's better to create a **Group** instead.
         """
