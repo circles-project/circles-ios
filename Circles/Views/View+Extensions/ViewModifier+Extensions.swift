11e5696a140537ca12c7913ae16b47da4904a7c8
Dmytro <Dmytro.Ryshchuk@futo.org>
2024-06-07 09:39:56 -0500

    Add View+Extensions folder

--- Circles/Views/View+Extensions/ViewModifier+Extensions.swift
+++ Circles/Views/View+Extensions/ViewModifier+Extensions.swift
@@ -1,23 +1,11 @@
 //
-//  View+Extensions.swift
+//  ViewModifier+Extensions.swift
 //  Circles
 //
 //  Created by Dmytro Ryshchuk on 6/6/24.
 //
 
 import SwiftUI
-
-struct BigBlueButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .frame(width: 300.0, height: 40.0)
-            .foregroundColor(.white)
-            .background(configuration.isPressed ? Color.accentColor.opacity(0.8) : Color.accentColor)
-            .cornerRadius(10)
-            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
-    }
-}
 
 struct CustomEmailTextFieldStyle: ViewModifier {
     var contentType: UITextContentType
