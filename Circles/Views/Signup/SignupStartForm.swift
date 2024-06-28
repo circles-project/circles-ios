a085048d85752be372b59b8aa631a80f4052de2b
Dmytro <Dmytro.Ryshchuk@futo.org>
2024-06-28 14:02:03 -0500

    372: Remove Subscription screen from registration flow

--- Circles/Views/Signup/SignupStartForm.swift
+++ Circles/Views/Signup/SignupStartForm.swift
@@ -10,81 +10,31 @@ import StoreKit
 import Matrix
 
 struct SignupStartForm: View {
-    //var matrix: MatrixInterface
     @ObservedObject var session: SignupSession
-    var store: CirclesStore
-    //@Binding var selectedScreen: LoggedOutScreen.Screen
     var state: UIAA.SessionState
-
-    //@Binding var selectedFlow: UIAA.Flow?
-
+    
     var body: some View {
         VStack {
-            Text("Sign up for Circles")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding()
-
-            Spacer()
-                .frame(minHeight: 100)
-
-            if let freeFlow = state.flows.first(where: { flow in
-                flow.stages.contains(AUTH_TYPE_FREE_SUBSCRIPTION) &&
-                    !flow.stages.contains(AUTH_TYPE_GOOGLE_SUBSCRIPTION) &&
-                    !flow.stages.contains(AUTH_TYPE_APPSTORE_SUBSCRIPTION)
-            }) {
-                AsyncButton(action: {
-                    //selectedFlow = tokenFlow
-                    await session.selectFlow(flow: freeFlow)
-                }) {
-                    Text("Sign up for free")
-                }
-                .buttonStyle(BigBlueButtonStyle())
-            } else {
-                Label("Subscriptionless signup is not available at this time. Please try again later.", systemImage: SystemImages.exclamationmarkTriangle.rawValue)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
-                Button(action: {}) {
-                    Text("Sign up for free")
-                }
-                .buttonStyle(BigBlueButtonStyle())
-                .disabled(true)
-            }
-
-
-            Spacer()
-                .frame(minHeight: 20)
-
-            let appleFlow = state.flows.first(where: {
-                $0.stages.contains(AUTH_TYPE_APPSTORE_SUBSCRIPTION)
-            })
-            if appleFlow == nil {
-                Label("New paid subscriptions are currently unavailable. Please try again later.", systemImage: SystemImages.exclamationmarkTriangle.rawValue)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
-            }
-            AsyncButton(action: {
-                //selectedFlow = appleFlow
-                await session.selectFlow(flow: appleFlow!)
-            }) {
-                Text("New Circles subscription")
-            }
-            .buttonStyle(BigBlueButtonStyle())
-            .disabled( nil == appleFlow || !SKPaymentQueue.canMakePayments() )
-            
-            if DebugModel.shared.debugMode {
-                VStack {
-                    let flows = state.flows
-                    ForEach(flows, id: \.self) { flow in
-                        Text("Auth flow:")
-                            .font(.body)
-                        ForEach(flow.stages, id: \.self) { stage in
-                            Text(stage)
-                                .font(.caption)
+            Color.clear
+                .onAppear {
+                    Task {
+                        if let freeFlow = state.flows.first(where: { flow in
+                            flow.stages.contains(AUTH_TYPE_FREE_SUBSCRIPTION) &&
+                            !flow.stages.contains(AUTH_TYPE_GOOGLE_SUBSCRIPTION) &&
+                            !flow.stages.contains(AUTH_TYPE_APPSTORE_SUBSCRIPTION)
+                        }) {
+                            await session.selectFlow(flow: freeFlow)
+                        } else {
+                            let appleFlow = state.flows.first(where: {
+                                $0.stages.contains(AUTH_TYPE_APPSTORE_SUBSCRIPTION)
+                            })
+                            
+                            if appleFlow != nil {
+                                await session.selectFlow(flow: appleFlow!)
+                            }
                         }
                     }
                 }
-            }
         }
     }
 }
