951098691fbf48087a128b681cd038c3f680a581
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-08-20 10:24:20 -0500

    Update app description text

--- Circles/Views/Login/AppHelpView.swift
+++ Circles/Views/Login/AppHelpView.swift
@@ -11,22 +11,7 @@ import MarkdownUI
 struct AppHelpView: View {
     
     let helpTextMarkdown = """
-        # Our mission
-                
-        We wanted the social features of Facebook with the security and privacy of Signal, so we built Circles.
-        
-        Circles helps you stay connected with family, friends, and your community, all with the safety of end-to-end encryption.
-        
-        Share your moments, thoughts, and memories safely with those closest to you, without worrying about who else can see.
-        
-        Post to your timeline to share with all of your people in one place.
-        
-        Connect with neighbors, coworkers, and friends from every sphere of life.
-        
-        Circles doesn't track you and doesn't show you ads. We work for you, not for advertisers.
-        Your data belongs to you. You can never be locked in to a single provider.
-        
-        All posts in Circles are encrypted using the same cryptography from state-of-the-art secure messaging tools like Signal and Matrix.
+        Circle is a secure social app where you can store and share your best moments with your loved ones without worrying that your data will be sold to anyone.
         """
     
     var body: some View {
